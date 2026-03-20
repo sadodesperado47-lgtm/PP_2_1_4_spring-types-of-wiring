b792dea345655a9c06bdb2fe3a9b908bed3eb153
Andrey T. <troshin_a_a@mail.ru>
2026-03-20 18:21:28 +0300

    Koshei's dead

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class Island2 {
 
     private Wood3 wood;
 
+    @Autowired
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
